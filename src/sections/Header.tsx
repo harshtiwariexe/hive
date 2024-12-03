f876c354032c9d6b6f162aa9fe478224e00486fc
Harsh Tiwari <harsh.official1802@gmail.com>
2024-12-03 20:02:30 +0530

    "Landing Page Done"

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import Logo2 from "@/assets/logo.png";
 import Image from "next/image";
@@ -8,29 +8,27 @@ import { SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
 export const Header = () => {
-  const router = useRouter()
-  const {isAuthenticated,isLoading} = useConvexAuth()
-
+  const router = useRouter();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
-
-   
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">Streamline your workflow and boost your productvity</p>
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productvity
+        </p>
         <div className="inline-flex gap-1 items-center">
-        {!isAuthenticated && !isLoading && (
-          <>
-            <SignInButton mode="modal">
-              <div className="flex cursor-pointer items-center"><p>Get started for free</p>
-                <ArrowRight className="h-4 w-4 ml-1 inline-flex justify-center items-center" />
-              </div>
-            </SignInButton>
-          </>
-        )}
-          
+          {!isAuthenticated && !isLoading && (
+            <>
+              <SignInButton mode="modal">
+                <div className="flex cursor-pointer items-center">
+                  <p>Get started for free</p>
+                  <ArrowRight className="h-4 w-4 ml-1 inline-flex justify-center items-center" />
+                </div>
+              </SignInButton>
+            </>
+          )}
         </div>
       </div>
 
@@ -47,11 +45,11 @@ export const Header = () => {
               <a href="">Help</a>
               {!isAuthenticated && !isLoading && (
                 <>
-                <SignInButton mode="modal">
-                <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight">
-                Sign In
-              </button>
-                </SignInButton>
+                  <SignInButton mode="modal">
+                    <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight">
+                      Sign In
+                    </button>
+                  </SignInButton>
                 </>
               )}
               {isAuthenticated && !isLoading && (
@@ -59,8 +57,8 @@ export const Header = () => {
                   <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight">
                     <Link href="/test">Enter Hive</Link>
                   </button>
-                  <SignOutButton/>
-                  <UserButton/>
+
+                  <UserButton />
                 </>
               )}
             </nav>
